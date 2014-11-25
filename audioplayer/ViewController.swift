904711277a3a5c824980abf234622969d7d69b0c
neodym1337 <johan.hosk@gmail.com>
2014-11-25 17:58:47 +0100

    Initial Commit

---
+++ audioplayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  audioplayer
+//
+//  Created by Johan Hosk on 25/11/14.
+//  Copyright (c) 2014 Johan Hosk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
